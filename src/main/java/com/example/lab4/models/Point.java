1f3c439e1266d3bfb446099ee59b465dec276e78
German Ganys <gganys@emphasoft.com>
2020-12-14 00:06:30 +0300

    Some fixes

--- src/main/java/com/example/lab4/models/Point.java
+++ src/main/java/com/example/lab4/models/Point.java
@@ -72,11 +72,11 @@ public class Point {
     }
 
     public static Boolean calculate(Double x, Double y, Double r) {
-        if (x >= 0 && y >= 0 && y <= r / 2 && x <= r)
+        if (x >= 0 && y >= 0 && x*x + y*y <= r*r)
             return true;
-        else if (x <= 0 && y >= 0 && y <= r && y <= x + r)
+        else if (x <= 0 && y >= 0 && y <= r/2 && x >= -r)
             return true;
-        else if (x >= 0 && y <= 0 && x*x + y*y <= r*r/4)
+        else if (x >= 0 && y <= 0 && y >= x - r/2)
             return true;
         else
             return false;
